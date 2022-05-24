21f7b704457f2dd3b4a383847dfdfd900c5f536b
Harry Sanders <harry.sandersiv@yahoo.com>
2022-05-23 22:11:50 -0400

    added home page css to create deals card

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,15 @@
 import Deals from '../components/Deals';
 
+import './Home.css';
+
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <div className="deals-card">
         <Deals />
-      {/* popular choices 
-      careers "Work for us" */}
+        {/* careers "Work for us" */}
+      </div>
     </>
   );
 };
