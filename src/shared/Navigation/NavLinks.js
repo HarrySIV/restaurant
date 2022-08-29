5a04c4f8fa01eb87a6fc7b3017b6d083f563a7dc
Harry Sanders <harry.sandersiv@yahoo.com>
2022-08-29 15:21:56 -0400

    changing deals to look more professional

--- src/shared/Navigation/NavLinks.js
+++ src/shared/Navigation/NavLinks.js
@@ -4,21 +4,20 @@ const NavLinks = () => {
   return (
     <ul className="nav-links">
       <li>
-        <NavLink to="/">
-          HOME
-        </NavLink>
+        <NavLink to="/">HOME</NavLink>
       </li>
       <li>
-        <NavLink to="/menu">
-          MENU
-        </NavLink>
+        <NavLink to="/menu">MENU</NavLink>
       </li>
       <li>
         <NavLink to="/about">ABOUT</NavLink>
       </li>
       <li>
-        <NavLink to="/locations">
-          LOCATIONS
+        <NavLink to="/locations">LOCATIONS</NavLink>
+      </li>
+      <li>
+        <NavLink to="/checkout">
+          <i class="fa-solid fa-cash-register"></i>
         </NavLink>
       </li>
     </ul>
