9d12ec69865f8373a702805fab8de618309debdd
Harry Sanders <harry.sandersiv@yahoo.com>
2022-09-04 22:19:27 -0400

    removed duplicate files and updated package.json

--- src/shared/hooks/menu-hook.tsx
+++ src/shared/hooks/menu-hook.tsx
@@ -1,4 +1,16 @@
-export const useMenu = () => {
-   
+import React, { useState } from 'react';
+import axios from 'axios';
+
+interface Item {
+  name: string;
+  description: string;
+  price: number;
+  _id: string;
+  cooking_time: string;
 }
 
+export const useMenu = () => {
+  const backendURL: string = 'https://localhost:3001/api/menu';
+  const [retrievedData, setRetrievedData] = useState<Item[]>();
+  const [message, setMessage] = useState<{ message: string }>();
+};
