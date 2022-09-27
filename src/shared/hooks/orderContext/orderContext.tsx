1e26890af4f1e9e59c223a853470e90d7e5bb291
Harry Sanders <harry.sandersiv@yahoo.com>
2022-09-27 00:52:44 -0400

    huge logical updates for order context

--- src/shared/hooks/orderContext/orderContext.tsx
+++ src/shared/hooks/orderContext/orderContext.tsx
@@ -2,57 +2,50 @@ import React, { createContext, useContext, useReducer } from 'react';
 import { IMenuItem } from '../database/menu-hook';
 import { orderReducer } from './orderReducer';
 
-const OrderContext = createContext<IMenuItem[]>([
-  {
-    name: '',
-    description: '',
-    price: 0,
-    _id: -1,
-    cooking_time: '',
-    hasToppings: false,
-    hasSizes: false,
-  },
-]);
+export interface IOrderContext {
+  items: { menuItem: IMenuItem; quantity: number }[];
+  total: number;
+}
+
+const initialOrderState: IOrderContext = { items: [], total: 0 };
+
+const OrderContext = createContext<IOrderContext>(initialOrderState);
 
 export const OrderProvider = ({ children }: { children: React.ReactNode }) => {
-  const [order, dispatch] = useReducer(orderReducer, []);
+  const [order, dispatch] = useReducer(orderReducer, initialOrderState);
 
-  const addToOrder = (item) => {
-    const updatedOrder = order.items.concat(item);
-    dispatch({
-      type: 'ADD_ITEM',
-      payload: {
-        order: updatedOrder,
-      },
-    });
+  const addToOrder = (item: IMenuItem) => {
+    const newOrder = Object.assign({}, order);
+    dispatch({ type: 'ADD_ITEM', orderContext: newOrder, item: item });
   };
 
-  const deleteFromOrder = (item) => {
-    const updatedOrder = order.items.filter(
-      (currentItem) => currentItem.id !== item.id
-    );
-    dispatch({
-      type: 'DELETE_ITEM',
-      payload: {
-        order: updatedOrder,
-      },
-    });
+  const deleteFromOrder = (item: IMenuItem) => {
+    const newOrder = Object.assign({}, order);
+    dispatch({ type: 'REMOVE_ITEM', orderContext: newOrder, item: item });
   };
 
-  // const clearOrder = () => {
-  //   dispatch({
-  //     type: 'CLEAR_ORDER',
-  //     payload: {
-  //       order: [],
-  //     },
-  //   });
-  // };
+  const updateItem = (item: IMenuItem) => {
+    const newOrder = Object.assign({}, order);
+    dispatch({ type: 'REMOVE_ITEM', orderContext: newOrder, item: item });
+  };
+
+  const clearOrder = () => {
+    dispatch({ type: 'CLEAR_ORDER', orderContext: order });
+  };
+
+  const updatePrice = () => {
+    let total = 0;
+    order.items.forEach((item) => (total += item.menuItem.price));
+    dispatch({ type: 'UPDATE_PRICE', total: total });
+  };
 
   const value = {
     order: order.items,
     addToOrder,
     deleteFromOrder,
-    // clearOrder,
+    updateItem,
+    clearOrder,
+    updatePrice,
   };
 
   return (
@@ -64,6 +57,6 @@ export const useOrderContext = () => {
   const context = useContext(OrderContext);
 
   if (context === undefined)
-    throw new Error('useOrder must be within OrderContext');
+    throw new Error('useOrder must only be used within OrderContext');
   return context;
 };
--- src/shared/hooks/orderContext/orderReducer.tsx
+++ src/shared/hooks/orderContext/orderReducer.tsx
@@ -1,31 +1,57 @@
 import { Reducer } from 'react';
 import { IMenuItem } from '../database/menu-hook';
+import { IOrderContext } from './OrderContext';
 
-export type OrderAction = { actionType: string; menuItem: IMenuItem };
+export type OrderAction =
+  | {
+      type: 'ADD_ITEM' | 'REMOVE_ITEM' | 'UPDATE_QUANTITY';
+      orderContext: IOrderContext;
+      item: IMenuItem;
+    }
+  | {
+      type: 'CLEAR_ORDER';
+      orderContext: IOrderContext;
+    }
+  | {
+      type: 'UPDATE_PRICE';
+      total: number;
+    };
 
-export const orderReducer: Reducer<IMenuItem[], OrderAction> = (
+export const orderReducer: Reducer<IOrderContext, OrderAction> = (
   orderReducerState,
   orderReducerAction
 ) => {
-  switch (orderReducerAction.actionType) {
+  switch (orderReducerAction.type) {
     case 'ADD_ITEM':
-      return [...orderReducerState, orderReducerAction.menuItem];
-    case 'DELETE_ITEM':
-      return orderReducerState.forEach((menuItem) => {
-        if (orderReducerAction.menuItem._id === menuItem._id) return;
-        return menuItem;
-      });
-    case 'UPDATE_ITEM':
-      return orderReducerState.forEach((menuItem) => {
-        if (orderReducerAction.menuItem._id === menuItem._id)
-          return { ...orderReducerAction.menuItem };
-        return menuItem;
-      });
-    // case 'CLEAR_ORDER':
-    //   return [];
+      //if item is in the order, update item quantity in newOrder
+      //if item is not in the order, add item to newOrder
+      return {
+        ...orderReducerState,
+        items: orderReducerAction.orderContext.items,
+      };
+    case 'REMOVE_ITEM':
+      //if item is in the order, update item quantity in newOrder to = 0
+      return {
+        ...orderReducerState,
+        items: orderReducerAction.orderContext.items,
+      };
+    case 'UPDATE_QUANTITY':
+      //find item id and set quantity to new quantity
+      return {
+        ...orderReducerState,
+        items: orderReducerAction.orderContext.items,
+      };
+    case 'CLEAR_ORDER':
+      //const newOrder = Object.assign({}, order);
+      //newOrder = items: [] and price: 0
+      return;
+    case 'UPDATE_PRICE':
+      return {
+        ...orderReducerState,
+        total: orderReducerAction.total,
+      };
     default:
       return orderReducerState;
   }
+  //orderReducerState.forEach(item) if(!item.quantity)  delete from array
 };
-
-// export const orderInitializer = [];
