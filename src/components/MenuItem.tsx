81521331dc8d0a9c01d3f4058e7b6a07cb5939ef
Harry Sanders <harry.sandersiv@yahoo.com>
2022-10-09 02:42:39 -0400

    menu style and modal open changes

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import { Button } from '../shared/elements/formElements/Button';
 import { useMenu, IMenuItem } from '../shared/hooks/database/menu-hook';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { AddToOrder } from './AddToOrder';
 
 export const MenuItem = () => {
   const [ID, setID] = useState<string>();
   const { menu } = useMenu();
+  const [openOrder, setOpenOrder] = useState<boolean>(false);
+  const [item, setItem] = useState<IMenuItem[]>([]);
 
   //onClick of menu item, displays menu item description
-  const itemHandler = (item: IMenuItem) => {
+  const descriptionHandler = (item: IMenuItem) => {
     if (ID === null || ID !== item._id) {
       setID(item._id);
     } else {
@@ -14,26 +20,48 @@ export const MenuItem = () => {
     }
   };
 
+  const addHandler = (item: IMenuItem) => {
+    setItem([item]);
+    setOpenOrder(true);
+  };
+
+  const closeHandler = () => {
+    setOpenOrder(false);
+    setItem([]);
+  };
+
   //displays menu items when menu and menu.length exist... breaks otherwise.
   return (
-    <ul className="items">
-      {menu.length &&
-        menu.map((item: IMenuItem) => {
-          return (
-            <li key={item._id} className="list-item">
-              <button className="menu-item" onClick={() => itemHandler(item)}>
-                {`${item.name} $${item.price}`}
-              </button>
-              <h4
-                className={`menu-description ${
-                  ID === item._id ? 'active-item' : 'none'
-                }`}
-              >
-                {item.description}
-              </h4>
-            </li>
-          );
-        })}
-    </ul>
+    <>
+      <ul className="items">
+        {menu.length &&
+          menu.map((item: IMenuItem) => {
+            return (
+              <li key={item._id} className="list-item">
+                <div className="li-inner">
+                  <button
+                    className="menu-item"
+                    onClick={() => descriptionHandler(item)}
+                  >
+                    {`${item.name} $${item.price}`}
+                  </button>
+                  <Button
+                    text={<FontAwesomeIcon icon={faPlus} />}
+                    onClick={() => addHandler(item)}
+                  />
+                </div>
+                <h4
+                  className={`menu-description ${
+                    ID === item._id ? 'active-item' : 'none'
+                  }`}
+                >
+                  {item.description}
+                </h4>
+              </li>
+            );
+          })}
+      </ul>
+      {openOrder && <AddToOrder items={item} closeHandler={closeHandler} />}
+    </>
   );
 };
