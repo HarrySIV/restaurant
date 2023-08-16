6acd7edab41c09737858de29cbc50cc082bf714d
Harry Sanders <harry.sandersiv@yahoo.com>
2023-08-16 11:59:47 -0400

    add deal to order WIP

--- src/components/AddDealToOrder.tsx
+++ src/components/AddDealToOrder.tsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+
 import { IDeal } from '../shared/hooks/database/deal-hook';
+import { useOrderContext } from '../shared/hooks/orderContext/OrderContext';
+import { useForm } from '../shared/hooks/form-hook';
+import { Modal } from '../shared/elements/uiElements/Modal';
+import { ItemInputs } from '../shared/elements/ItemInputs';
 
 interface IAddDealToOrderProps {
   deal: IDeal;
@@ -6,5 +12,42 @@ interface IAddDealToOrderProps {
 }
 
 export const AddDealToOrder = (props: IAddDealToOrderProps) => {
-  return <h1>deal</h1>;
+  const orderContext = useOrderContext();
+  const [total, setTotal] = useState(props.deal.total);
+  const [formState, inputHandler] = useForm({}, true);
+
+  const dealSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (props.deal) {
+      orderContext.addToOrder({
+        item: props.deal.items,
+        quantity: 1,
+        total: props.deal.total,
+      });
+    }
+  };
+
+  return (
+    <Modal header="Add to Order" closeHandler={props.closeHandler}>
+      <form className="order-form" onSubmit={dealSubmitHandler}>
+        {props.deal && (
+          <div key={props.deal._id}>
+            <legend>{props.deal.name}</legend>
+            {props.deal.items.map((item) => (
+              <ItemInputs
+                id={`${props.deal._id}`}
+                deal={props.deal}
+                inputHandler={inputHandler}
+                // totalHandler={totalHandler}
+                initialValue={props.initialValue}
+                quantity={quantity}
+                setQuantity={setQuantity}
+                disabled={false}
+              ></ItemInputs>
+            ))}
+          </div>
+        )}
+      </form>
+    </Modal>
+  );
 };
